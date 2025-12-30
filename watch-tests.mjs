6e996712ca1f277cfa2b31c39019b3fd942aa7e2
SiripireddyGiri <Siripireddygirikumar3@gmail.com>
2025-12-30 09:22:46 +0530

    fix: removed unwanted echo's

--- watch-tests.mjs
+++ watch-tests.mjs
@@ -17,13 +17,14 @@ const cssWatcher = chokidar.watch(["css/**/*.css"], {
 });
 
 
+
 // Helper function to run a command with live stdio
+
 const runCommand = (cmd, args, onClose) => {
   const proc = spawn(cmd, args, { stdio: "inherit" });
   proc.on("close", onClose);
 };
 
-// JS watcher
 jsWatcher.on("change", (path) => {
   console.log(`\nJS File changed: ${path}`);
   console.log("Running ESLint...\n");
@@ -48,7 +49,6 @@ jsWatcher.on("change", (path) => {
   });
 });
 
-// CSS watcher
 cssWatcher.on("change", (path) => {
   console.log(`\nCSS File changed: ${path}`);
   console.log("Running Stylelint...\n");
