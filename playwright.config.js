7f54ea37d2796b0404f6d1d67bf563feee9dfac3
SiripireddyGiri <Siripireddygirikumar3@gmail.com>
2026-01-05 20:13:11 +0530

    fix: added no.of workers in playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./__tests__/visual",
   outputDir: "test-results",
-  fullyParallel: false,
+  fullyParallel: true, 
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: 1,
+  workers: process.env.CI ? 2 : 4,
   reporter: [
     ["html", { outputFolder: "playwright-report", open: "never" }],
     ["list"],
