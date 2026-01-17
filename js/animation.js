e3c5a7e825437c06fe2e5c394b715a61f5535b15
Shoaib232002 <shoaib232002@gmail.com>
2026-01-17 21:02:45 +0530

    feat: addition of keyframes info in the editor

--- js/animation.js
+++ js/animation.js
@@ -4,19 +4,3 @@ const styleSheet = document.createElement("style");
 document.head.appendChild(styleSheet);
 
 styleSheet.textContent = ANIMATION_CONSTANTS.KEYFRAMES_CSS;
-
-export const AnimationModule = {
-  getAnimationNames() {
-    return ANIMATION_CONSTANTS.KEYFRAME_NAMES;
-  },
-
-  isValidAnimation(name) {
-    return this.getAnimationNames().includes(name.toLowerCase());
-  },
-
-  getKeyframeDefinitions() {
-    return ANIMATION_CONSTANTS.KEYFRAMES_CSS;
-  },
-};
-
-export default AnimationModule;
